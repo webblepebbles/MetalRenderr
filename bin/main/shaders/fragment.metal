85627aa53dad06a1dffa5b89a9d667364669671d
NewUsername <helloidkidclol@proton.me>
2026-02-12 13:19:58 +1100

    v0.1.7 preview (not complete guys some bugs still and i'll fix before v0.1.7's .jar)

--- bin/main/shaders/fragment.metal
+++
@@ -1,9 +0,0 @@
-#include <metal_stdlib>
-using namespace metal;
-
-fragment float4 fragment_main(half3 normal [[stage_in]]) {
-    half3 lightDir = normalize(half3(0.5, 1.0, 0.7));
-    float diff = max(dot(normal, lightDir), 0.2);
-    half3 color = half3(0.7, 0.8, 1.0) * diff;
-    return float4(color, 1.0);
-}
--- compile_native.sh
+++ compile_native.sh
@@ -2,12 +2,31 @@
 set -e
 
 echo "Compiling shaders..."
-# Compile terrain shader
-xcrun -sdk macosx metal -c src/main/resources/native/shaders/metalrender.metal -o src/main/resources/native/shaders/metalrender.air
-# Compile entity shader
-xcrun -sdk macosx metal -c src/main/resources/native/shaders/entity.metal -o src/main/resources/native/shaders/entity.air
-# Link both into metallib
-xcrun -sdk macosx metallib src/main/resources/native/shaders/metalrender.air src/main/resources/native/shaders/entity.air -o src/main/resources/shaders.metallib
+SHADER_DIR="src/main/resources/native/shaders"
+
+xcrun -sdk macosx metal -c $SHADER_DIR/metalrender.metal -o $SHADER_DIR/metalrender.air
+xcrun -sdk macosx metal -c $SHADER_DIR/entity.metal -o $SHADER_DIR/entity.air
+xcrun -sdk macosx metal -c $SHADER_DIR/culling.metal -o $SHADER_DIR/culling.air
+xcrun -sdk macosx metal -c $SHADER_DIR/hiz.metal -o $SHADER_DIR/hiz.air
+xcrun -sdk macosx metal -c $SHADER_DIR/occlusion_culling.metal -o $SHADER_DIR/occlusion_culling.air
+xcrun -sdk macosx metal -c $SHADER_DIR/lod_select.metal -o $SHADER_DIR/lod_select.air
+xcrun -sdk macosx metal -c $SHADER_DIR/visibility_buffer.metal -o $SHADER_DIR/visibility_buffer.air
+xcrun -sdk macosx metal -c $SHADER_DIR/oit_transparency.metal -o $SHADER_DIR/oit_transparency.air
+xcrun -sdk macosx metal -c $SHADER_DIR/lod_terrain_extended.metal -o $SHADER_DIR/lod_terrain_extended.air
+xcrun -sdk macosx metal -c $SHADER_DIR/lod_compute_mesher.metal -o $SHADER_DIR/lod_compute_mesher.air
+
+xcrun -sdk macosx metallib \
+    $SHADER_DIR/metalrender.air \
+    $SHADER_DIR/entity.air \
+    $SHADER_DIR/culling.air \
+    $SHADER_DIR/hiz.air \
+    $SHADER_DIR/occlusion_culling.air \
+    $SHADER_DIR/lod_select.air \
+    $SHADER_DIR/visibility_buffer.air \
+    $SHADER_DIR/oit_transparency.air \
+    $SHADER_DIR/lod_terrain_extended.air \
+    $SHADER_DIR/lod_compute_mesher.air \
+    -o src/main/resources/shaders.metallib
 echo "Shaders compiled to src/main/resources/shaders.metallib"
 
 echo "Compiling native library..."
@@ -15,7 +34,8 @@ JAVA_HOME=$(/usr/libexec/java_home)
 echo "Using JAVA_HOME: $JAVA_HOME"
 
 clang++ -O3 -std=c++17 -dynamiclib \
-    -framework Metal -framework Foundation -framework Cocoa -framework IOKit -framework IOSurface -framework OpenGL -framework QuartzCore \
+    -DMETALRENDER_HAS_METALFX=1 \
+    -framework Metal -framework MetalFX -framework Foundation -framework Cocoa -framework IOKit -framework IOSurface -framework OpenGL -framework QuartzCore \
     -I"$JAVA_HOME/include" -I"$JAVA_HOME/include/darwin" \
     -I"src/main/resources/native" \
     src/main/resources/native/metalrender.mm \
