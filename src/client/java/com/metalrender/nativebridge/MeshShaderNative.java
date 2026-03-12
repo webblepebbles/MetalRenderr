a45edb18e8a8e166dc9f980597778ee473045f04
NewUsername <226301485+webblepebbles@users.noreply.github.com>
2026-03-12 20:17:16 +1100

    Deleted files

--- src/client/java/com/metalrender/nativebridge/MeshShaderNative.java
+++
@@ -1,7 +0,0 @@
-package com.metalrender.nativebridge;
-
-public final class MeshShaderNative {
-    private MeshShaderNative() {}
-
-    public static native void dispatchTerrain(long handle, int visibleRegions, long indirectBufferAddr);
-}
