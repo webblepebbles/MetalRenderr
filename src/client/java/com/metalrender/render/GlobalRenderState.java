c9b79ec76cfdf58eccca29a96d9aba7c62855305
NewUsername <226301485+webblepebbles@users.noreply.github.com>
2026-03-13 17:24:54 +1100

    Removed files!

--- src/client/java/com/metalrender/render/GlobalRenderState.java
+++
@@ -1,6 +0,0 @@
-package com.metalrender.render;
-
-public class GlobalRenderState {
-    public static volatile int atlasWidth = 1024;
-    public static volatile int atlasHeight = 1024;
-}
--- src/client/java/com/metalrender/sodium/format/MetalChunkMeshFormats.java
+++
@@ -1,8 +0,0 @@
-package com.metalrender.sodium.format;
-
-import net.caffeinemc.mods.sodium.client.render.chunk.vertex.format.ChunkVertexType;
-
-public class MetalChunkMeshFormats {
-    
-    public static final ChunkVertexType METAL = MetalChunkVertex.INSTANCE;
-}
